a288e9bb15fe565b2f4895b8707bf95df5868f98
gigacool <cedric.hartland@gmail.com>
2016-12-17 23:50:24 +0100

    repair unit test configuration

--- karma.entry.js
+++ karma.entry.js
@@ -5,7 +5,7 @@ var exclude = [
   './main.js'
 ];
 
-var context = require.context('./src', true, /\.js$/);
+var context = require.context('./sources', true, /\.js$/);
 
 context.keys().forEach(function(key) {
   if (exclude.indexOf(key) === -1) context(key);
